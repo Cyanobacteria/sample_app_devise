f46900df2bf3201ff296a48e61a3ed7d0110f4fc
cyanobacteria <vodomelkor@gmail.com>
2017-07-30 03:39:02 +0800

    paperclip with

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :microposts
   has_one :profile
+  
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+
 end
--- db/migrate/20170728234041_add_column_avatar_to_profiles.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnAvatarToProfiles < ActiveRecord::Migration[5.1]
-  def up
-    add_attachment :profiles, :avatar
-  end
-
-  def down
-    remove_attachment :profiles, :avatar
-  end
-end
